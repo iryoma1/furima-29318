5e78ea39fdcd9db297852bc850c1fcfcf9bfd0d7
iryoma1 <69664360+iryoma1@users.noreply.github.com>
2020-09-14 16:04:03 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_data])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_data])
   end
 
   def basic_auth
@@ -14,6 +15,5 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,20 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  PASSWORD_REGEX = /\A[a-z\d]{6,}+\z/
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
-  
+
+  validates :nickname, presence: true
+  validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, format: {with: PASSWORD_REGEX }
+  validates :first_name, presence: true, format: { with: NAME_REGEX }
+  validates :last_name, presence: true, format: { with: NAME_REGEX }
+  validates :first_name_kana, presence: true, format: { with: NAME_KANA_REGEX }
+  validates :last_name_kana, presence: true, format: { with: NAME_KANA_REGEX } 
+  validates :birth_data, presence: true
+
 end
+
