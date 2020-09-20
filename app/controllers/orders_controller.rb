dc085c70f83e084f075bd668cfa0a12a683939c1
iryoma1 <69664360+iryoma1@users.noreply.github.com>
2020-09-20 12:45:58 +0900

    API導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
   def index
+    @items = Item.all
+    @item = Item.find(params[:item_id])
+    if @item.user.id == current_user.id
+      redirect_to root_path
+    end
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :purchase
   belongs_to_active_hash :prefecture
+
+  POSTAL_CODE_REGEX = /^\d{3}[-]\d{4}$/
+  PHONR_NUMBER_REGEX = /^\d{11}$/
+
+  with_options presence: true do
+    validates :postal_code, format { with: POSTAL_CODE_REGEX}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format { with: PHONR_NUMBER_REGEX}
+    validates :purchase
+  end
 end
